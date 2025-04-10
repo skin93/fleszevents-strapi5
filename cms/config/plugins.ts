af219fc3e8ddc27c1d2031a135c008a3826b86cc
skin93 <arro.ciesielski@gmail.com>
2025-04-10 19:22:15 +0200

    Install plugins for cms

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,1 +1,17 @@
-export default () => ({});
+export default () => ({
+  seo: {
+    enabled: true,
+  },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      landingPage: false, // disable Sandbox everywhere
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+});
