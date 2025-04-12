91ca75807b5470f098d87ae9383404ab1916d3ed
skin93 <arro.ciesielski@gmail.com>
2025-04-12 19:54:48 +0200

    Add web-converter

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -4,13 +4,22 @@ export default () => ({
   },
   "gen-types": {
     enabled: true,
-
     config: {
       outputLocation: "myTypes.ts",
-
-      // If this is true, then the outputLocation should be the location to a .ts file
-
       singleFile: true,
+    },
+  },
+  "webp-converter": {
+    enabled: true,
+
+    config: {
+      // mimeTypes that converts to WebP. Default is ['image/png', 'image/jpeg', 'image/jpg']
+
+      mimeTypes: undefined,
+
+      options: {
+        // WebP options: https://sharp.pixelplumbing.com/api-output#webp
+      },
     },
   },
   graphql: {
