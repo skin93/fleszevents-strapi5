c0916686e7111184361ccd734a88242feae1a479
skin93 <arro.ciesielski@gmail.com>
2025-07-01 21:14:07 +0200

    Fetch all articles

--- client/lib/queries/articles/articlesSitemapQuery.ts
+++ client/lib/queries/articles/articlesSitemapQuery.ts
@@ -1,7 +1,7 @@
 import { gql } from "graphql-request";
 export const ARTICLES_SITEMAP_QUERY = gql`
   query {
-    articles(sort: "updatedAt:DESC") {
+    articles(sort: "updatedAt:DESC", pagination: { limit: -1 }) {
       documentId
       title
       slug
