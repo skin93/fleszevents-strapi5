80f950b665a010711c418054e2702f858c60b17d
skin93 <arro.ciesielski@gmail.com>
2025-07-01 21:57:45 +0200

    Add headers

--- client/next.config.ts
+++ client/next.config.ts
@@ -16,6 +16,43 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: `${process.env.NEXT_PUBLIC_APP_DOMAIN}`,
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "no-referrer",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=(self), microphone=(), camera=()",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Server-Timing",
+            value: "app; dur=100",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
