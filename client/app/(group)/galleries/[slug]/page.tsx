f80157616633234ef1bfcfbc63cafe351c84ec45
skin93 <arro.ciesielski@gmail.com>
2025-07-12 12:23:25 +0200

    Open image on new blank

--- client/app/(group)/galleries/[slug]/page.tsx
+++ client/app/(group)/galleries/[slug]/page.tsx
@@ -4,13 +4,14 @@ import { Metadata } from "next";
 import Image from "next/image";
 import { getMediaUrl } from "@/lib/getMediaUrl";
 import { getGalleryBySlug, getGalleryMeta } from "@/lib/data/galleries";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+// import {
+//   Dialog,
+//   DialogContent,
+//   DialogHeader,
+//   DialogTitle,
+//   DialogTrigger,
+// } from "@/components/ui/dialog";
+import Link from "next/link";
 
 export async function generateMetadata({
   params,
@@ -85,9 +86,37 @@ export default async function GallerySlugPage({
               {column?.map((photo) => {
                 const src = getMediaUrl(photo);
                 return (
-                  <Dialog key={photo.documentId}>
-                    <DialogTrigger asChild className="cursor-pointer">
-                      <div className="relative translate-y-0  hover:translate-y-2 transition-all duration-300">
+                  // <Dialog key={photo.documentId}>
+                  //   <DialogTrigger asChild className="cursor-pointer">
+                  //     <div className="relative translate-y-0  hover:translate-y-2 transition-all duration-300">
+                  //       <Image
+                  //         priority
+                  //         width={photo.width}
+                  //         height={photo.height}
+                  //         src={src}
+                  //         alt={photo.alternativeText}
+                  //         className="rounded-lg object-cover relative"
+                  //         sizes="(min-width: 1560px) 435px, (min-width: 1280px) calc(15.38vw + 198px), (min-width: 640px) calc(50vw - 40px), (min-width: 460px) calc(100vw - 64px), calc(7.86vw + 341px)"
+                  //       />
+                  //     </div>
+                  //   </DialogTrigger>
+                  //   <DialogContent className="border-0 max-w-[70vw] h-auto p-0 ">
+                  //     <DialogHeader className="hidden">
+                  //       <DialogTitle>{photo.name}</DialogTitle>
+                  //     </DialogHeader>
+                  //     <Image
+                  //       priority
+                  //       width={photo.width}
+                  //       height={photo.height}
+                  //       src={src}
+                  //       alt={photo.alternativeText}
+                  //       className="rounded-lg object-contain "
+                  //     />
+                  //   </DialogContent>
+                  // </Dialog>
+                  <div key={photo.documentId}>
+                    <div className="relative translate-y-0  hover:translate-y-2 transition-all duration-300 cursor-pointer">
+                      <Link href={src} target="_blank">
                         <Image
                           priority
                           width={photo.width}
@@ -97,22 +126,9 @@ export default async function GallerySlugPage({
                           className="rounded-lg object-cover relative"
                           sizes="(min-width: 1560px) 435px, (min-width: 1280px) calc(15.38vw + 198px), (min-width: 640px) calc(50vw - 40px), (min-width: 460px) calc(100vw - 64px), calc(7.86vw + 341px)"
                         />
-                      </div>
-                    </DialogTrigger>
-                    <DialogContent className="border-0 max-w-[70rem] h-auto p-0 ">
-                      <DialogHeader className="hidden">
-                        <DialogTitle>{photo.name}</DialogTitle>
-                      </DialogHeader>
-                      <Image
-                        priority
-                        width={photo.width}
-                        height={photo.height}
-                        src={src}
-                        alt={photo.alternativeText}
-                        className="rounded-lg object-contain"
-                      />
-                    </DialogContent>
-                  </Dialog>
+                      </Link>
+                    </div>
+                  </div>
                 );
               })}
             </div>
