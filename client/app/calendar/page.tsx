bfc6b34a3343297024c717e517dd51752da3ad02
skin93 <arro.ciesielski@gmail.com>
2025-09-23 18:17:10 +0200

    Force pre-rendering CalendarPage

--- client/app/calendar/page.tsx
+++ client/app/calendar/page.tsx
@@ -1,8 +1,9 @@
 import CustomCalendar from "@/components/custom-calendar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { getAllEvents } from "@/lib/data/events";
+export const dynamic = "force-dynamic";
 
-export default async function Page() {
+export default async function CalendarPage() {
   const { events } = await getAllEvents();
   return (
     <SidebarProvider>
