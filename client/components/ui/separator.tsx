24fe7f13851124ca4541b3ce6e044121b7ceafc9
skin93 <arro.ciesielski@gmail.com>
2025-12-07 22:00:46 +0100

    Change blockquote style & separator color

--- client/components/ui/separator.tsx
+++ client/components/ui/separator.tsx
@@ -17,7 +17,7 @@ function Separator({
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-[5px] data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-[5px] data-[orientation=vertical]:w-px",
+        "bg-accent shrink-0 data-[orientation=horizontal]:h-[5px] data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-[5px] data-[orientation=vertical]:w-px",
         className
       )}
       {...props}
