109f992debd8d0e85267869d6f2a27da448d3db2
skin93 <arro.ciesielski@gmail.com>
2025-12-18 13:33:35 +0100

    Query all dates after today

--- client/lib/queries/events/allDatesQuery.ts
+++ client/lib/queries/events/allDatesQuery.ts
@@ -1,8 +1,8 @@
 import { gql } from "graphql-request";
 
 export const ALL_DATES_QUERY = gql`
-  query allDatesQuery {
-    events(pagination: { limit: -1 }) {
+  query allDatesQuery($date: Date!) {
+    events(filters: { date: { gte: $date } }, pagination: { limit: -1 }) {
       date
     }
   }
