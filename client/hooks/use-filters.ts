1707f2603a4b4ae2ce6bb6c59e27c8f27cc74b07
skin93 <arro.ciesielski@gmail.com>
2025-12-18 13:06:29 +0100

    Set filters for calendar

--- client/hooks/use-filters.ts
+++ client/hooks/use-filters.ts
@@ -1,11 +1,13 @@
-// hooks/use-filters.ts
 "use client";
 
 import { useQueryState, createParser } from "nuqs";
 import {
   festivalsSearchParamsSchema,
   festivalsSearchParams,
+  calendarSearchParams,
+  calendarSearchParamsSchema,
 } from "@/lib/validation";
+import { format, parseISO } from "date-fns";
 
 const zodParserForMap = (key: keyof festivalsSearchParams) =>
   createParser({
@@ -50,5 +52,55 @@ export function useMapFilters() {
     setCity,
     setFest,
     setGenre,
+  };
+}
+
+const zodParserForCalendar = (key: keyof calendarSearchParams) =>
+  createParser({
+    parse: (value) => {
+      const result = calendarSearchParamsSchema.shape[key].safeParse(value);
+      return result.success ? result.data : "";
+    },
+    serialize: (value) => value.toString(),
+  });
+
+const dateParser = createParser({
+  parse: (value) => parseISO(value),
+  serialize: (value) => format(value, "yyyy-MM-dd"),
+});
+
+export function useCalendarFilters() {
+  const [city, setCity] = useQueryState(
+    "city",
+    zodParserForCalendar("city")
+      .withOptions({
+        shallow: false,
+        history: "push",
+      })
+      .withDefault("")
+  );
+  const [location, setLocation] = useQueryState(
+    "location",
+    zodParserForCalendar("location")
+      .withOptions({
+        shallow: false,
+        history: "push",
+      })
+      .withDefault("")
+  );
+
+  const [date, setDate] = useQueryState(
+    "date",
+    dateParser
+      .withOptions({ shallow: false, history: "push", clearOnDefault: true })
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .withDefault(null as any) // Domyślnie brak wybranej daty
+  );
+
+  return {
+    filters: { city, location, date },
+    setCity,
+    setLocation,
+    setDate,
   };
 }
