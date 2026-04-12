68e31d6c9662522d0ada5bf2af5c4bba89967ada
skin93 <arro.ciesielski@gmail.com>
2026-04-12 15:09:48 +0200

    Add jsonld script to the galleries page

--- client/app/(group)/galleries/page.tsx
+++ client/app/(group)/galleries/page.tsx
@@ -4,11 +4,13 @@ import CustomPagination from "@/components/ui/custom/pagination";
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import GalleryCard from "@/components/ui/custom/gallery-card";
+import { WebPage, WithContext } from "schema-dts";
+import { Fragment } from "react";
 
 export const metadata: Metadata = {
   title: "Galerie",
-  description: "Fotorelacje z koncertów i festiwali!",
-  keywords: ["galerie", "fotorelacje"],
+  description:
+    "Obszerne fotorelacje i galerie zdjęć z koncertów oraz festiwali rockowych i metalowych!",
   robots: {
     index: true,
     follow: true,
@@ -43,6 +45,48 @@ type Props = {
 };
 
 export default async function GalleriesPage({ searchParams }: Props) {
+  const jsonLd: WithContext<WebPage> = {
+    "@context": "https://schema.org",
+    "@type": "CollectionPage",
+    mainEntityOfPage: {
+      "@type": "WebPage",
+      "@id": "https://fleszevents.pl/galleries",
+    },
+    name: "Galerie zdjęć - FleszEvents",
+    description:
+      "Obszerne fotorelacje i galerie zdjęć z koncertów oraz festiwali rockowych i metalowych!",
+    breadcrumb: {
+      "@type": "BreadcrumbList",
+      itemListElement: [
+        {
+          "@type": "ListItem",
+          position: 1,
+          name: "FleszEvents",
+          item: "https://fleszevents.pl/",
+        },
+        {
+          "@type": "ListItem",
+          position: 2,
+          name: "Galerie",
+          item: "https://fleszevents.pl/galleries",
+        },
+      ],
+    },
+    publisher: {
+      "@type": "Organization",
+      name: "FleszEvents",
+      logo: {
+        "@type": "ImageObject",
+        url: "https://fleszevents.pl/FE_1_baner.svg",
+      },
+    },
+    about: {
+      "@type": "ImageGallery",
+      description:
+        "Zbiór fotografii dokumentujących polską scenę rockową i metalową!.",
+    },
+  };
+
   const { page } = await searchParams;
   const currentPage = Number(page) || 1;
   const { galleries, pageInfo } = await getGalleries(currentPage, 12);
@@ -52,24 +96,32 @@ export default async function GalleriesPage({ searchParams }: Props) {
   }
 
   return (
-    <main>
-      <section aria-label="Galleries">
-        <h1 className="my-8 text-center uppercase">GALERIE</h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 ">
-          {galleries.map((gallery) => (
-            <div key={gallery.documentId}>
-              <Link href={`/galleries/${gallery.slug}`}>
-                <GalleryCard gallery={gallery} />
-              </Link>
-            </div>
-          ))}
-        </div>
-        <div className="my-8" />
-        <CustomPagination
-          currentPage={currentPage}
-          pageCount={pageInfo.pageCount}
-        />
-      </section>
-    </main>
+    <Fragment>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLd).replace(/</g, "\\u003c"),
+        }}
+      />
+      <main>
+        <section aria-label="Galleries">
+          <h1 className="my-8 text-center uppercase">GALERIE</h1>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 ">
+            {galleries.map((gallery) => (
+              <div key={gallery.documentId}>
+                <Link href={`/galleries/${gallery.slug}`}>
+                  <GalleryCard gallery={gallery} />
+                </Link>
+              </div>
+            ))}
+          </div>
+          <div className="my-8" />
+          <CustomPagination
+            currentPage={currentPage}
+            pageCount={pageInfo.pageCount}
+          />
+        </section>
+      </main>
+    </Fragment>
   );
 }
