bef26933f9a44a6121116e6afeec74486677b46f
skin93 <arro.ciesielski@gmail.com>
2026-04-12 16:04:25 +0200

    Add jsonld script to the single tag page

--- client/app/(group)/tags/[slug]/page.tsx
+++ client/app/(group)/tags/[slug]/page.tsx
@@ -1,10 +1,11 @@
 import BaseCard from "@/components/ui/custom/base-card";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import React from "react";
+import React, { Fragment } from "react";
 import CustomPagination from "@/components/ui/custom/pagination";
 import { getArticlesByTag, getTag } from "@/lib/data/tags";
 import { Metadata } from "next";
+import { CollectionPage, WithContext } from "schema-dts";
 
 type Props = {
   params: Promise<{ slug: string }>;
@@ -49,28 +50,80 @@ export default async function TagPage({ params, searchParams }: Props) {
     notFound();
   }
 
+  const jsonLd: WithContext<CollectionPage> = {
+    "@context": "https://schema.org",
+    "@type": "CollectionPage",
+    mainEntityOfPage: {
+      "@type": "WebPage",
+      "@id": "https://fleszevents.pl/tags/4dziki",
+    },
+    name: `${tag.name} - wiadomości, koncerty, relacje | FleszEvents`,
+    description: `Wszystkie informacje na temat ${tag.name} w jednym miejscu. Artykuły, zapowiedzi koncertów, fotorelacje oraz aktualności powiązane z tagiem ${tag.name} w serwisie FleszEvents.`,
+    breadcrumb: {
+      "@type": "BreadcrumbList",
+      itemListElement: [
+        {
+          "@type": "ListItem",
+          position: 1,
+          name: "FleszEvents",
+          item: "https://fleszevents.pl/",
+        },
+        {
+          "@type": "ListItem",
+          position: 2,
+          name: "Tagi",
+          item: "https://fleszevents.pl/tags",
+        },
+        {
+          "@type": "ListItem",
+          position: 3,
+          name: `${tag.name}`,
+          item: `https://fleszevents.pl/tags/${tag.slug}`,
+        },
+      ],
+    },
+    publisher: {
+      "@type": "Organization",
+      name: "FleszEvents",
+      logo: {
+        "@type": "ImageObject",
+        url: "https://fleszevents.pl/FE_1_baner.svg",
+      },
+    },
+  };
+
   return (
-    <main>
-      <section
-        aria-label={`${slug} content`}
-        className="flex flex-col justify-center items-center"
-      >
-        <h1 className="my-8 text-center uppercase">{tag.name.toUpperCase()}</h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3">
-          {articles.map((article) => (
-            <div key={article.documentId}>
-              <Link href={`/articles/${article.slug}`}>
-                <BaseCard article={article} />
-              </Link>
-            </div>
-          ))}
-        </div>
-        <div className="m-8" />
-        <CustomPagination
-          currentPage={currentPage}
-          pageCount={pageInfo.pageCount}
-        />
-      </section>
-    </main>
+    <Fragment>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(jsonLd).replace(/</g, "\\u003c"),
+        }}
+      />
+      <main>
+        <section
+          aria-label={`${slug} content`}
+          className="flex flex-col justify-center items-center"
+        >
+          <h1 className="my-8 text-center uppercase">
+            {tag.name.toUpperCase()}
+          </h1>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3">
+            {articles.map((article) => (
+              <div key={article.documentId}>
+                <Link href={`/articles/${article.slug}`}>
+                  <BaseCard article={article} />
+                </Link>
+              </div>
+            ))}
+          </div>
+          <div className="m-8" />
+          <CustomPagination
+            currentPage={currentPage}
+            pageCount={pageInfo.pageCount}
+          />
+        </section>
+      </main>
+    </Fragment>
   );
 }
