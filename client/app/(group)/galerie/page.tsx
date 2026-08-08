b9da9a1ce0d1df434012bb3b102a5a626992c8fe
skin93 <arro.ciesielski@gmail.com>
2026-08-08 13:31:14 +0200

    Display 3 grid cols on lg screen in gallery

--- client/app/(group)/galerie/page.tsx
+++ client/app/(group)/galerie/page.tsx
@@ -133,7 +133,7 @@ export default async function GalleriesPage({ searchParams }: Props) {
             </Breadcrumb>
           </div>
           <h1 className="my-8 text-center uppercase">GALERIE</h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 ">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 ">
             {galleries.map((gallery) => (
               <div key={gallery.documentId}>
                 <Link href={`/galerie/${gallery.slug}`}>
